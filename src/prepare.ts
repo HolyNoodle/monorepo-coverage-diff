f6854c65fd3a492dfba2530f7a9c57f66a753d1a
HolyNoodle <kbriffaut@gmail.com>
2023-09-23 21:00:42 +0200

    feat: rework message and code

--- src/prepare.ts
+++ src/prepare.ts
@@ -3,24 +3,18 @@ import { execSync } from 'child_process'
 import { Folders } from './types'
 
 export async function prepare(commands: string[], folders: Folders) {
-  const promises = Object.values(folders).map(folder => {
-    return new Promise(async resolve => {
-      for (const command of commands) {
-        core.info(`${folder}: ${command}`)
-        try {
-          execSync(command, { cwd: folder })
-        } catch (error: any) {
-          core.error(error.message)
-        }
-      }
-      resolve(undefined)
-    })
-  })
+  for (const folder of Object.values(folders)) {
+    for (const command of commands) {
+      core.info(`${folder}: ${command}`)
+      try {
+        const buffer = execSync(command, { cwd: folder })
 
-  try {
-    await Promise.all(promises)
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) console.error(error.message)
+        core.info(buffer.toString())
+      } catch (error: any) {
+        if (error instanceof Error) core.setFailed(error.message)
+
+        core.error(error.toString())
+      }
+    }
   }
 }
